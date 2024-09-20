77e68de5cb001e0d35af41f9028adf5230254baa
BojanPetrov <bojan8petrov@gmail.com>
2024-09-20 15:41:18 +0200

    mrc

--- src/handlers/ManageService.java
+++ src/handlers/ManageService.java
@@ -1,5 +1,17 @@
 package handlers;
 
-public class ManageService {
+import keys.NativeKeyboard;
 
+public class ManageService {
+	
+	private NativeKeyboard keyboard;
+	
+	public ManageService() {
+	// TODO Auto-generated constructor stub
+		keyboard = new NativeKeyboard();
+	}
+	
+	public NativeKeyboard getKeyboard() {
+		return keyboard;
+	}
 }
--- src/main/main.java
+++ src/main/main.java
@@ -1,10 +1,25 @@
 package main;
 
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+
+import handlers.ManageService;
+
+
 public class main {
+	
 
 	public static void main(String[] args) {
+		
+		ManageService service = new ManageService();
 		// TODO Auto-generated method stub
-
+		try {
+			GlobalScreen.registerNativeHook();
+		} catch (NativeHookException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		GlobalScreen.getInstance().addNativeKeyListener(service.getKeyboard());
 	}
 
 }
